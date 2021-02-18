f8630c66991ab35b319353e2bf3b5a41eb36b9b1
sebastian.diers <sebastian.diers@syntaxphoenix.com>
2021-02-18 03:02:13 +0100

    feat: cache & requestTimeout

--- src/BGPView.php
+++ src/BGPView.php
@@ -3,69 +3,112 @@
 namespace SyntaxPhoenix\Api\BGPView;
 
 use GuzzleHttp\Client;
-use Psr\Http\Message\ResponseInterface;
+use Nette\Caching\Cache;
+use Nette\Caching\Storage;
+use Nette\Caching\Storages\FileStorage;
 use SyntaxPhoenix\Api\BGPView\Exceptions\RequestFailedException;
 
 class BGPView
 {
 
     private string $baseUrl;
+    private bool $caching;
     private Client $client;
+    private Storage $storage;
+    private Cache $cache;
+    private string $cacheTime;
+    private float $lastRequestTimestamp = 0;
+    private float $requestTimeout;
 
-    public function __construct(string $baseUrl) {
+    public function __construct(string $baseUrl, float $requestTimeout = 0.4, bool $caching = false, ?string $cachingUrl = 'web/cache/bgpview', ?string $cacheTime = '10 minutes') {
+        if ($caching) {
+            $this->createPath($cachingUrl);
+            $this->storage = new FileStorage($cachingUrl);
+            $this->cache = new Cache($this->storage, 'bgpview-requests');
+        }
         $this->baseUrl = $baseUrl;
+        $this->caching = $caching;
+        $this->cacheTime = $cacheTime;
+        $this->requestTimeout = $requestTimeout;
         $this->client = new Client([
-            'base_uri' => $baseUrl,
-            'timeout'  => 2.0,
+            'base_uri' => $baseUrl
         ]);
     }
 
     public function getAsnDetails(int $asNumber): array
     {
-        return json_decode($this->getDataByApi('asn/' . $asNumber)->getBody(), true)['data'];
+        return $this->getDataByApi('asn/' . $asNumber)['data'];
     }
 
     public function getAsnPrefixes(int $asNumber): array
     {
-        return json_decode($this->getDataByApi('asn/' . $asNumber . '/prefixes')->getBody(), true)['data'];
+        return $this->getDataByApi('asn/' . $asNumber . '/prefixes')['data'];
     }
 
     public function getAsnPeers(int $asNumber): array
     {
-        return json_decode($this->getDataByApi('asn/' . $asNumber . '/peers')->getBody(), true)['data'];
+        return $this->getDataByApi('asn/' . $asNumber . '/peers')['data'];
     }
 
     public function getAsnUpstreams(int $asNumber): array
     {
-        return json_decode($this->getDataByApi('asn/' . $asNumber . '/upstreams')->getBody(), true)['data'];
+        return $this->getDataByApi('asn/' . $asNumber . '/upstreams')['data'];
     }
 
     public function getAsnDownstreams(int $asNumber): array
     {
-        return json_decode($this->getDataByApi('asn/' . $asNumber . '/downstreams')->getBody(), true)['data'];
+        return $this->getDataByApi('asn/' . $asNumber . '/downstreams')['data'];
     }
 
     public function getPrefix(string $ipAddress, int $cidr): array
     {
-        return json_decode($this->getDataByApi('prefix/' . $ipAddress . '/' . $cidr)->getBody(), true)['data'];
+        return $this->getDataByApi('prefix/' . $ipAddress . '/' . $cidr)['data'];
     }
 
     public function getIPDetails(string $ipAddress): array
     {
-        return json_decode($this->getDataByApi('prefix/ip/' . $ipAddress)->getBody(), true)['data'];
+        return $this->getDataByApi('ip/' . $ipAddress)['data'];
     }
 
     public function getIXDetails(int $ixId): array
     {
-        return json_decode($this->getDataByApi('ix/' . $ixId)->getBody(), true)['data'];
+        return $this->getDataByApi('ix/' . $ixId)['data'];
     }
 
-    private function getDataByApi(string $urlPart): ResponseInterface
+    private function getDataByApi(string $urlPart): array
     {
+        if ($this->caching) {
+            $response = $this->cache->load($urlPart);
+            if ($response) {
+                return $response;
+            }
+        }
+        $time = microtime(true);
+        if (($this->lastRequestTimestamp + $this->requestTimeout) > $time) {
+            $restTime = $this->requestTimeout - ($time - $this->lastRequestTimestamp);
+            usleep($restTime * 1000 * 1000);
+        }
         $response = $this->client->request('GET', $urlPart);
         if ($response->getStatusCode() != 200) {
             throw new RequestFailedException();
         }
-        return $response;
+        $finalResponse = json_decode($response->getBody(), true);
+        if ($this->caching) {
+            $this->cache->save($urlPart, $finalResponse, [
+                Cache::EXPIRE => $this->cacheTime ?? '10 minutes',
+            ]);
+        }
+        $this->lastRequestTimestamp = microtime(true);
+
+        return $finalResponse;
+    }
+
+    private function createPath($path) {
+        if (is_dir($path)) {
+            return true;
+        }
+        $prev_path = substr($path, 0, strrpos($path, '/', -2) + 1 );
+        $return = $this->createPath($prev_path);
+        return ($return && is_writable($prev_path)) ? mkdir($path) : false;
     }
 }
